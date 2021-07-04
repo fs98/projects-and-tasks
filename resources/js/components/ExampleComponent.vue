a5de4f600e2ce139306596d8b7e63a61bbef3fc5
Fata <sefer.fata@gmail.com>
2021-07-04 23:32:33 +0200

    Implemented Vue.js and Tailwind.css

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga tenetur est asperiores molestiae, blanditiis exercitationem esse velit explicabo sequi, fugiat ullam! Veniam, cumque! Eveniet aperiam nesciunt odit culpa soluta laboriosam!
+  </div>
+</template>
--- webpack.mix.js
+++ webpack.mix.js
@@ -11,8 +11,9 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
+mix.js('resources/js/app.js', 'public/js').vue()
     .postCss('resources/css/app.css', 'public/css', [
-        //
+        require('tailwindcss'),
+        require('postcss-import')
     ])
-    .browserSync('127.0.0.1:8000');;
+    .browserSync('127.0.0.1:8000');
